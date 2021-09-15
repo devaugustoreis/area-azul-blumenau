7c4f858789faf04011f964396c088c140f3dcebe
Marina <marina.macieski@outlook.com>
2021-09-15 16:32:18 -0300

    Atualização página "esqueci_senha"

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('esqueci_senha/', views.esqueci_senha, name='esqueci_senha')
 ]
