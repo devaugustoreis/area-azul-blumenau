620dd77d603c762e38c6fd848b3e58bc575e95ca
Augusto Reis <devaugustoreis@gmail.com>
2021-09-17 17:10:05 -0300

    Criação da página base do app Clientes e definição dos caminhos para cada página do app Clientes..

--- cliente/urls.py
+++ cliente/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name="index"),
+    path("estacionar/", views.estacionar, name="estacionar"),
+    path("extrato/", views.extrato, name="extrato"),
+    path("creditos/", views.creditos, name="creditos"),
+    path("multas/", views.multas, name="multas"),
+    path("veiculos/", views.veiculos, name="veiculos"),
+    path("dados/", views.dados, name="dados"),
 ]
