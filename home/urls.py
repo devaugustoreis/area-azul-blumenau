6cd51cab74880f4c312b3b234df0ea1f6a5dcdb0
DevAugustoReis <devaugustoreis@gmail.com>
2021-09-22 12:13:34 -0300

    Parcial revisões app Home

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
     path('esqueci_senha/', views.esqueci_senha, name='esqueci_senha')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'home/index.html')
+def login(request):
+    return render(request, 'home/login.html')
 
 def esqueci_senha(request):
     return render(request, 'home/esqueci_senha.html')
