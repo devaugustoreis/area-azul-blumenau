9121f29bdf93c045100c999b4f060b95b7a004ac
Augusto Reis <devaugustoreis@gmail.com>
2021-09-23 17:27:26 -0300

    Criação models e registro (Parcial)

---
+++ home/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
+
+
+class ClientForm(ModelForm):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class CreateUser(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['user', 'first_name', 'last_name', 'phone1', 'phone2', 'email', 'credits', 'password1', 'password2']
--- home/models.py
+++ home/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Address(models.Model):
+    zip_code = models.CharField(max_length=8)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    street_name = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=6)
+    complement = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    phone1 = models.CharField(max_length=20)
+    phone2 = models.CharField(max_length=20, null=True, blank=True)
+    email = models.CharField(max_length=100)
+    credits = models.FloatField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Vehicles(models.Model):
+    VEHICLES_TYPE = (
+        ('C', 'Car'),
+        ('M', 'Moto'),
+        ('O', 'Other'),
+    )
+    license_plate = models.CharField(max_length=7)
+    entry_time = models.DateTimeField(null=True ,blank=True)
+    timer = models.IntegerField(null=True, blank=True)
+    vehicle_type = models.CharField(max_length=1, choices=VEHICLES_TYPE)
+    client = models.ManyToManyField(Client)
+
+    def __str__(self):
+        return self.name
