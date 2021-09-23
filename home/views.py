19b14853f4b3376905ac2fb535749ed04d15bdb8
Augusto Reis <devaugustoreis@gmail.com>
2021-09-23 17:27:26 -0300

    Criação models e registro (Parcial)

--- home/views.py
+++ home/views.py
@@ -1,4 +1,53 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users, admin_only
+
+@unauthenticated_user
+def register(request):
+    form = CreateUser()
+    if request.method == 'POST':
+        form = CreateUser(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            Customer.objects.create(user=user,)
+
+            # messages.success(request, 'Usuário ' + username +
+            #                  ' cadastrado com sucesso.')
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Usuário ou senha incorretos.')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
 
 def index(request):
     return render(request, 'home/index.html')
