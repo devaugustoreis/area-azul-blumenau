1aae293933434b2df645b16524e275bc6bc241f4
DevAugustoReis <devaugustoreis@gmail.com>
2021-09-27 12:00:59 -0300

    Ajustes Models

--- home/forms.py
+++ home/forms.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 from .models import *
 
 
-class ClientForm(ModelForm):
-    class Meta:
-        model = Client
-        fields = '__all__'
+# class ClientForm(ModelForm):
+#     class Meta:
+#         model = Client
+#         fields = '__all__'
 
 
-class CreateUser(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['user', 'first_name', 'last_name', 'phone1', 'phone2', 'email', 'credits', 'password1', 'password2']
+# class CreateUser(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ['user', 'first_name', 'last_name', 'phone1', 'phone2', 'email', 'credits', 'password1', 'password2']
--- home/views.py
+++ home/views.py
@@ -1,52 +1,51 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from .decorators import unauthenticated_user, allowed_users, admin_only
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+# from .decorators import unauthenticated_user, allowed_users, admin_only
 
-@unauthenticated_user
-def register(request):
-    form = CreateUser()
-    if request.method == 'POST':
-        form = CreateUser(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
+# @unauthenticated_user
+# def register(request):
+#     form = CreateUser()
+#     if request.method == 'POST':
+#         form = CreateUser(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user,)
+#             group = Group.objects.get(name='customer')
+#             user.groups.add(group)
+#             Customer.objects.create(user=user,)
 
-            # messages.success(request, 'Usuário ' + username +
-            #                  ' cadastrado com sucesso.')
-            return redirect('login')
+#             # messages.success(request, 'Usuário ' + username +
+#             #                  ' cadastrado com sucesso.')
+#             return redirect('login')
 
-    context = {'form': form}
-    return render(request, 'accounts/register.html', context)
+#     context = {'form': form}
+#     return render(request, 'accounts/register.html', context)
 
-@unauthenticated_user
-def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+# @unauthenticated_user
+# def loginPage(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+#         user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Usuário ou senha incorretos.')
+#         if user is not None:
+#             login(request, user)
+#             return redirect('home')
+#         else:
+#             messages.info(request, 'Usuário ou senha incorretos.')
 
-    context = {}
-    return render(request, 'accounts/login.html', context)
+#     context = {}
+#     return render(request, 'accounts/login.html', context)
 
 
-def logoutUser(request):
-    logout(request)
-    return redirect('login')
-
+# def logoutUser(request):
+#     logout(request)
+#     return redirect('login')
 
 
 def login(request):
