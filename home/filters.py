8e10c71d4714984001ae6bf45ca766eb9840408c
DevAugustoReis <devaugustoreis@gmail.com>
2021-10-14 12:35:58 -0300

    Ajustes Finais 01

--- home/filters.py
+++ home/filters.py
@@ -1,20 +1,21 @@
 from django.db.models import fields
 import django_filters
-from django_filters.filters import NumberFilter
+from django_filters.filters import ModelChoiceFilter, ModelMultipleChoiceFilter, NumberFilter
 from .models import *
 from django_filters import DateFilter
 
-class VehicleFilter(django_filters.FilterSet):
-    class Meta:
-        model = Vehicle
-        fields = '__all__'
+# class VehicleFilter(django_filters.FilterSet):
+#     class Meta:
+#         model = Vehicle
+#         fields = '__all__'
 
 
 class OperationsFilter(django_filters.FilterSet):
     start_date = DateFilter(field_name="date", lookup_expr="gte")
     end_date = DateFilter(field_name="date", lookup_expr="lte")
+    vehicle = filters.ModelChoiceFilter(queryset=departments)
     start_value = NumberFilter(field_name="value", lookup_expr="gte")
     end_value = NumberFilter(field_name="value", lookup_expr="lte")
     class Meta:
         model = Operation
-        fields = ['operation_type', 'payment_method', 'vehicle']
+        fields = ['operation_type', 'payment_method']
